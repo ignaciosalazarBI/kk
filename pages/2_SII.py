b55ae51e835ff91ad63c5cb9e90b9eaa747db5fd
Ignacio Salazar Larraguibel <ignacio.salazar.larraguibel@gmail.com>
2026-08-21 20:52:42 -0400

    Use safe runtime navigation in SII

--- pages/2_SII.py
+++ pages/2_SII.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from beta_modules import render_sii
+from beta_runtime import render_sii
 
 st.set_page_config(page_title="Control Pyme · SII", page_icon="🧾", layout="wide")
 render_sii()
