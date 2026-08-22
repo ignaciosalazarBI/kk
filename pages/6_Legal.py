ef25dba0da489c5a4e0d18f3afe528052a73798c
Ignacio Salazar Larraguibel <ignacio.salazar.larraguibel@gmail.com>
2026-08-21 20:53:11 -0400

    Use safe runtime navigation in Legal

--- pages/6_Legal.py
+++ pages/6_Legal.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from beta_modules import render_legal
+from beta_runtime import render_legal
 
 st.set_page_config(page_title="Control Pyme · Legal", page_icon="⚖️", layout="wide")
 render_legal()
