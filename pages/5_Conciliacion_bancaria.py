4c59b97526c708729eccb8a4b77d13d947a5a73a
Ignacio Salazar Larraguibel <ignacio.salazar.larraguibel@gmail.com>
2026-08-21 20:53:03 -0400

    Use safe runtime navigation in Conciliacion

--- pages/5_Conciliacion_bancaria.py
+++ pages/5_Conciliacion_bancaria.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from beta_modules import render_conciliacion
+from beta_runtime import render_conciliacion
 
 st.set_page_config(page_title="Control Pyme · Conciliación", page_icon="🏦", layout="wide")
 render_conciliacion()
