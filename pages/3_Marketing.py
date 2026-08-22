98f82e0e4c55d238f83f026477f3e503ac0a71cc
Ignacio Salazar Larraguibel <ignacio.salazar.larraguibel@gmail.com>
2026-08-21 20:52:49 -0400

    Use safe runtime navigation in Marketing

--- pages/3_Marketing.py
+++ pages/3_Marketing.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from beta_modules import render_marketing
+from beta_runtime import render_marketing
 
 st.set_page_config(page_title="Control Pyme · Marketing", page_icon="📣", layout="wide")
 render_marketing()
